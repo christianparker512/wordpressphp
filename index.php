9e111c8de698462473c176701c70cdaf4b1ee19b
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-08-21 16:43:59 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+echo "# wordpressphp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/christianparker512/wordpressphp.git
+git push -u origin main
