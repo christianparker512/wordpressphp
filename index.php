175d32f1283e23cd352942d9d44a3aebad965b5b
Christian Parker <71667365+christianparker512@users.noreply.github.com>
2021-08-21 20:46:58 -0500

    header and footer

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-    $myName = "Chris"
-  ?>
+<?php get_header();
 
-  <h1>Hi, my nmae is <?php echo $myName; ?></h1>
+  
+  while (have_posts()){
+    the_post(); ?> 
+    <h2><a href="<?php the_permalink()?>"\><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+  <?php }
+
+get_footer();
+
+?>
+ 
